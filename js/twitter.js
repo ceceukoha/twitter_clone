dd7c646da00f5f19d7fdad67ceeaf629082205c9
ceceukoha <ceceukoha@gmail.com>
2023-01-31 20:25:23 -0500

    Segments of twitter clone

---
+++ js/twitter.js
@@ -0,0 +1,11 @@
+const profPic = document.querySelector('#prof');
+const closeIcon = document.querySelector('.close-icon');
+const overlayMenu = document.querySelector('.overlay');
+
+
+function openMenu{
+    overlayMenu.style.display = "block";
+    overlayMenu.style.display = "block";
+    overlayMenu.style.display = "block";
+    overlayMenu.style.display = "block";d
+}
